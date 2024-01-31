8e785c6e7fe58ab8f715447d733b9a7a7d45c0b8
Med Aziz Guennichi <medazizguennichi@gmail.com>
2024-01-31 12:48:09 -0800

    add the opening WLAN handler functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 use windows::{
   core::{GUID,HSTRING,PCWSTR,PWSTR},
   Win32::{
@@ -10,6 +11,46 @@ use windows::{
   },
 };
 
+fn open_wlan_handle(api_version:u32) -> Result<HANDLE,windows::core::Error> {
+  let mut negotiatied_version = 0;
+  let mut wlan_handle = INVALID_HANDLE_VALUE;
+
+  let result = unsafe {WlanOpenHandle(api_version, None, &mut negotiatied_version, &mut wlan_handle)};
+
+  WIN32_ERROR(result).ok()?;
+
+  Ok(wlan_handle)
+}
+
+fn enum_wlan_interfaces(handle:HANDLE) -> Result<*mut WLAN_INTERFACE_INFO_LIST,windows::core::Error> {
+  let mut interface_ptr = std::ptr::null_mut();
+
+  let result = unsafe {WlanEnumInterfaces(handle, None, &mut interface_ptr)};
+
+  WIN32_ERROR(result).ok()?;
+
+  Ok(interface_ptr)
+}
+
+fn grab_interface_profiles(handle:HANDLE,interface_guid:&GUID) -> Result<*const WLAN_PROFILE_INFO_LIST, windows::core::Error> {
+  let mut wlan_profiles_ptr = std::ptr::null_mut();
+
+  let result = unsafe {WlanGetProfileList(handle, interface_guid, None, &mut wlan_profiles_ptr)};
+
+  WIN32_ERROR(result).ok()?;
+
+  Ok(wlan_profiles_ptr)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let wlan_handle = open_wlan_handle(WLAN_API_VERSION_2_0).expect("Failed to open WLAN handle!");
+
+    let interface_ptr = match enum_wlan_interfaces(wlan_handle) {
+      Ok(interfaces) => interfaces,
+      Err(e) => {
+        eprintln!("Failed to enumerate interfaces: {}", e);
+        unsafe { WlanCloseHandle(wlan_handle, None) };
+        std::process::exit(1);
+      }
+    };
 }
