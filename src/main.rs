8005e86ffd24e4b7ef4646a1a7e9720342e76def
Med Aziz Guennichi <medazizguennichi@gmail.com>
2024-01-31 12:35:24 -0800

    add the dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use windows::{
+  core::{GUID,HSTRING,PCWSTR,PWSTR},
+  Win32::{
+    Foundation::{ERROR_SUCCESS,HANDLE,INVALID_HANDLE_VALUE,WIN32_ERROR},
+    NetworkManagement::WiFi::{
+      WlanCloseHandle, WlanEnumInterfaces, WlanFreeMemory, WlanGetProfile, WlanGetProfileList,
+      WlanOpenHandle,WLAN_API_VERSION_2_0, WLAN_INTERFACE_INFO_LIST,
+      WLAN_PROFILE_GET_PLAINTEXT_KEY, WLAN_PROFILE_INFO_LIST,
+    },
+  },
+};
+
 fn main() {
     println!("Hello, world!");
 }
